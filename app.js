b2006fd46e520583dfd899e2f299e003518035db
Esemega <sogaral89@gmail.com>
2020-10-05 19:08:18 +0200

    created funstions for each operator

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 //Create a function for each operator
 // There will be 2 inputs and 1 output
-
+const a = 3;
+const b = 7;
 //Sum
-
+const sum = (a, b) => a + b; 
 //Subtract
-
+const subtract =(a, b) => a - b;
 //Multiply
-
+const multiply = (a,b) => a * b;
 //Divide
+const divide = (a,b) => a/b;
+
+console.log(sum(a,b));
+console.log(subtract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
