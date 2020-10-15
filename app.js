fe19e18c90084baa33fb1c254c97579b805f6f79
Esemega <sogaral89@gmail.com>
2020-10-15 19:33:19 +0200

    adding functions calculate, getSymbolOfChosenOperation and printSequence, modifying getInput, and getResult

--- app.js
+++ app.js
@@ -1,43 +1,97 @@
-//Create a function for each operator
-// There will be 2 inputs and 1 output
-
 //basic functions sum, subtract, multiply & divide
 const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+//calculate depending on operation
+const calculate = (operation, num1, num2) => {
+  switch (operation) {
+    case "sum":
+      return sum(Number(num1), Number(num2));
+    case "subtract":
+      return subtract(num1, num2);
+    case "multiply":
+      return multiply(num1, num2);
+    case "divide":
+      return divide(num1, num2);
+  }
+};
+
+//get symbol of chosen operation
+const getSymbolOfChosenOperation = (operation) => {
+  switch (operation) {
+    case "sum":
+      return "+";
+    case "subtract":
+      return "-";
+    case "multiply":
+      return "x";
+    case "divide":
+      return "/";
+    case "equal":
+      return "=";
+  }
+};
+
 //get input
-const getInput = (input) => document.getElementById("input-" + input).value;
+const getInput = () => document.getElementById("input").value;
 
 //print the result in the output -> h3#result
 const setResult = (result) => {
   document.getElementById("result").innerText = result;
 };
 
+const printSequence = (sequence) => {
+  document.getElementById("operations").innerText = sequence;
+};
+
+//declaring this before the event handler
+let temp = 0;
+let lastChosenOperation;
+let operations = "";
 //Evaluate if any input is empty and run the function depending on button clicked
 const getResult = (event) => {
-  const a = getInput("a");
-  const b = getInput("b");
+  
+  //read input
+  const input = getInput();
+  //read chosen operation
+  let chosenOperation = event.target.id.replace("button-", "");
 
-  if (a === "" || b === "") {
-    setResult("Error!");
+  if (chosenOperation === "equal") {
+    temp = calculate(lastChosenOperation, temp, input);
+    setResult(temp);
+    // console.log("if chosenOperation === equal - temp", temp);
+    // console.log("if chosenOperation === equal - chosenOperation", chosenOperation);
+    // console.log("if chosenOperation === equal - lastChosenOperation", lastChosenOperation);
+    operations = operations + input +" "+ getSymbolOfChosenOperation(chosenOperation)+" ";
+    printSequence(operations);
+    operations = "";
+    lastChosenOperation = "";
+    temp = 0;
   } else {
-    switch (event.target.id) {
-      case "button-sum":
-        setResult(sum(Number(a), Number(b)));
-        break;
-      case "button-subtract":
-        setResult(subtract(a, b));
-        break;
-      case "button-multiply":
-        setResult(multiply(a, b));
-        break;
-      case "button-divide":
-        setResult(divide(a, b));
-        break;
+    if (temp === 0) {
+      temp = input;
+      setResult("");
+      // console.log("if temp === '' - temp", temp);
+      // console.log("if temp === '' - chosenOperation", chosenOperation);
+      // console.log("if temp === '' - lastChosenOperation", lastChosenOperation);
+    } else {
+      temp = calculate(lastChosenOperation, temp, input);
+      // console.log("else temp === '' - temp", temp);
+      // console.log("else temp === '' - chosenOperation", chosenOperation);
+      // console.log("else temp === '' - lastChosenOperation", lastChosenOperation);
     }
+    lastChosenOperation = chosenOperation;
+    operations = operations + input +" "+ getSymbolOfChosenOperation(chosenOperation)+" ";
+    printSequence(operations);
   }
+
+  //print all operations
+  // operations = operations + input + getSymbolOfChosenOperation(chosenOperation);
+  // printSequence(operations);
+  //clean the input
+  document.getElementById("input").value = "";
 };
 
 //Adding an event listerner for each button
@@ -51,3 +105,4 @@ addEventListernerToButton("sum");
 addEventListernerToButton("subtract");
 addEventListernerToButton("multiply");
 addEventListernerToButton("divide");
+addEventListernerToButton("equal");
